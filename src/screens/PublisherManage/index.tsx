102a92a1917f21b6e22c595e9ebdf19d294baf4d
Christiansly <abiode23@gmail.com>
2023-01-17 12:09:57 +0100

    publisher requests page

--- src/screens/PublisherManage/index.tsx
+++ src/screens/PublisherManage/index.tsx
@@ -8,6 +8,7 @@ import ActiveIcon from "../../components/svgs/ActiveIcon";
 import TabButton from "../../components/TabButton/TabButton";
 import AgencyPublishersTable from "./AgencyPublishersTable";
 import BrandPublishersTable from "./BrandPublishersTable";
+import AgencyPublishersRequest from "./AgencyPublisherRequest";
 
 function PublisherManage() {
   const [tabIndex, setTabIndex] = useState<0 | 1>(0);
@@ -35,9 +36,13 @@ function PublisherManage() {
     switch (userType) {
       case "Agency":
         if (tabIndex === 0) {
-          return (<AgencyPublishersTable />);
-        } else if(tabIndex === 1) {
-          return <div></div>
+          return <AgencyPublishersTable />;
+        } else if (tabIndex === 1) {
+          return (
+            <div>
+              <AgencyPublishersRequest />
+            </div>
+          );
         }
         break;
       case "Brand":
@@ -46,43 +51,42 @@ function PublisherManage() {
   };
 
   return (
-      <div className="row pt-3 ps-2 pe-5">
-        <div className="col-12 mt-3 d-flex justify-content-between">
-          <h4>{getTitle()}</h4>
-          {userType === "Agency" && (
-            <button className="btn btn-primary btn-sm">Connect</button>
-          )}
-        </div>
-        <div className="col-12 mt-4 d-flex">
+    <div className="row pt-3 ps-2 pe-5">
+      <div className="col-12 mt-3 d-flex justify-content-between">
+        <h4>{getTitle()}</h4>
+        {userType === "Agency" && (
+          <button className="btn btn-primary btn-sm">Connect</button>
+        )}
+      </div>
+      <div className="col-12 mt-4 d-flex">
+        <TabButton
+          text="Publishers"
+          onClick={() => {
+            setTabIndex(0);
+          }}
+          selected={tabIndex === 0}
+        />
+        {userType === "Agency" && (
           <TabButton
-            text="Publishers"
+            text="Publisher Requests"
             onClick={() => {
-              setTabIndex(0);
+              setTabIndex(1);
             }}
-            selected={tabIndex === 0}
+            selected={tabIndex === 1}
           />
-          {userType === "Agency" && (
-            <TabButton
-              text="Publisher Requests"
-              onClick={() => {
-                setTabIndex(1);
-              }}
-              selected={tabIndex === 1}
-            />
-          )}
-        </div>
-        <div className="col-12 mt-4">
-          <div className="card d-flex p-2">
-            <div className="row">
-              <span className="col d-inline d-flex align-items-center ">
-                <ListIcon />{" "}
-                <span className="fs-5 ms-2">{getTableHeader()}</span>
-              </span>
-            </div>
+        )}
+      </div>
+      <div className="col-12 mt-4">
+        <div className="card d-flex p-2">
+          <div className="row">
+            <span className="col d-inline d-flex align-items-center ">
+              <ListIcon /> <span className="fs-5 ms-2">{getTableHeader()}</span>
+            </span>
           </div>
         </div>
-        <div className="col-12">{getTableBody()}</div>
       </div>
+      <div className="col-12">{getTableBody()}</div>
+    </div>
   );
 }
 
