a0ffe27300f2c373c2e6eb4d62a777bba523d287
abba bawa <abbabawa@punch.cool>
2023-01-19 14:52:02 +0100

    My offers link changed

--- src/App.tsx
+++ src/App.tsx
@@ -85,7 +85,7 @@ function App() {
               />
               <Route
                 path="publisher/active-brands"
-                element={<ActiveBrands />}
+                element={<PaymentReport />}
               />
               <Route
                 path="publisher/brand-products"
